2d8065ed2368a9cba0562dc60f693ca74ee2fa8d
Diana <AdoXv_ma30@student.itstep.org>
2024-11-02 13:52:05 +0200

    added class rectangle

--- class9GIT/class9GIT.cpp
+++ class9GIT/class9GIT.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
+
+class Rectangle
+{
+
+};
 
 int main()
 {
     int i = 5;
     int j = 12;
+    //створення екземпляру класу rectangle
+    Rectangle r;
 }
